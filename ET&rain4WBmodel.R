ed6ec3025cdb18cbfd8773d21838b74738e6289d
Niranjan Acharige <nman2690@uni.sydney.edu.au>
2023-10-20 17:34:29 +1100

    Add files via upload

--- ET&rain4WBmodel.R
+++ ET&rain4WBmodel.R
@@ -1,6 +1,5 @@
-## preprocess the MODIS ET 500m and SILO rain 5km
 
-#8-day downloaded MODIS ET
+#8-day ET
 setwd("evapotranspiration/ET2001_2022")
 ET.list <- list.files(pattern =".tif", full.names=F)
 ET.stack<-raster::stack(ET.list)
@@ -34,14 +33,11 @@ f <- function(x) {
   }
   x
 }
+
 dailyET <- calc(sub, f)
+
 ################################
-# downloaded daily SILO rain
+# daily rain
 rain_list <- list.files(path="rain", pattern ="*.nc", full.names=TRUE)
 rain <- raster::stack(rain_list)
-#######################################################################
-# downscale to the SLGA soil years (theta at 90 m resolusion)
-ET<- raster::resample(x=dailyET, y=theta, method = "bilinear")
-rain<- raster::resample(x=rain, y=thata, method = "ngb")
-
 
